7af3a0a4a6e747e5dbc44f66f52bf5e450087c07
zirra <msullivan@heysulli.com>
2018-12-20 10:04:41 -0600

    update

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ var options = {
 app.use(express.static('app', options));
 
 
-app.use('/admin', express.static(__dirname + '/admin/', options) );
+app.use('/', express.static(__dirname + '/wwwroot/', options) );
 
 app.use('/cdn', express.static(__dirname + '/cdn/') );
 
